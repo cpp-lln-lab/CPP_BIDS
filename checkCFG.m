b5068a43fdc790d42e768665d047608c4652ae71
Remi Gau <remi_gau@hotmail.com>
2020-07-23 16:58:11 +0200

    create unit test for checkCFG

--- checkCFG.m
+++ checkCFG.m
@@ -11,19 +11,34 @@ function [cfg, expParameters] = checkCFG(cfg, expParameters)
         '..', ...
         'output');
 
+    fieldsToSet = mriDefaults(fieldsToSet);
+
     fieldsToSet.subjectGrp = ''; % in case no group was provided
     fieldsToSet.sessionNb = 1; % in case no session was provided
     fieldsToSet.askGrpSess = [true true];
 
-    % BIDS
-    fieldsToSet = datasetDescriptionDefaults(fieldsToSet);
-    fieldsToSet = mriDefaults(fieldsToSet);
+    expParameters = setDefaultFields(expParameters, fieldsToSet);
 
+    %% BIDS
+    clear fieldsToSet;
+    fieldsToSet = struct();
+    fieldsToSet = datasetDescriptionDefaults(fieldsToSet);
+
+    expParameters.bids.datasetDescription = ...
+        setDefaultFields(expParameters.bids.datasetDescription, fieldsToSet);
+
+    clear fieldsToSet;
+    fieldsToSet = struct();
+    fieldsToSet = mriJsonDefaults(fieldsToSet);
     if isfield(expParameters, 'task')
-        fieldsToSet.bids.MRI.TaskName = expParameters.task;
+        fieldsToSet.TaskName = expParameters.task;
     end
 
-    expParameters = setDefaultFields(expParameters, fieldsToSet);
+    expParameters.bids.MRI = ...
+        setDefaultFields(expParameters.bids.MRI, fieldsToSet);
+
+    % sort fields alphabetically
+    expParameters = orderfields(expParameters);
 
     %% set the cfg defaults
 
@@ -38,33 +53,7 @@ function [cfg, expParameters] = checkCFG(cfg, expParameters)
 
 end
 
-function fieldsToSet = datasetDescriptionDefaults(fieldsToSet)
-    % required
-    fieldsToSet.bids.datasetDescription.json.Name = '';
-    fieldsToSet.bids.datasetDescription.json.BIDSVersion = '';
-    % recommended
-    fieldsToSet.bids.datasetDescription.json.License = '';
-    fieldsToSet.bids.datasetDescription.json.Authors = {''};
-    fieldsToSet.bids.datasetDescription.json.Acknowledgements = '';
-    fieldsToSet.bids.datasetDescription.json.HowToAcknowledge = '';
-    fieldsToSet.bids.datasetDescription.json.Funding = {''};
-    fieldsToSet.bids.datasetDescription.json.ReferencesAndLinks = {''};
-    fieldsToSet.bids.datasetDescription.json.DatasetDOI = '';
-end
-
 function fieldsToSet = mriDefaults(fieldsToSet)
-
-    % for json for funcfional data
-    % required
-    fieldsToSet.bids.MRI.RepetitionTime = [];
-    fieldsToSet.bids.MRI.SliceTiming = '';
-    fieldsToSet.bids.MRI.TaskName = '';
-    fieldsToSet.bids.MRI.PhaseEncodingDirection = '';
-    fieldsToSet.bids.MRI.EffectiveEchoSpacing = '';
-    fieldsToSet.bids.MRI.EchoTime = '';
-    % recommended
-    fieldsToSet.bids.MRI.Instructions = '';
-    fieldsToSet.bids.MRI.TaskDescription = '';
 
     % for file naming
     fieldsToSet.MRI.ce = [];
@@ -72,5 +61,35 @@ function fieldsToSet = mriDefaults(fieldsToSet)
     fieldsToSet.MRI.rec = []; % reconstruction of fMRI images
     fieldsToSet.MRI.echo = []; % echo fMRI images
     fieldsToSet.MRI.acq = []; % acquisition of fMRI images
+
+end
+
+function fieldsToSet = datasetDescriptionDefaults(fieldsToSet)
+    % required
+    fieldsToSet.Name = '';
+    fieldsToSet.BIDSVersion = '';
+    % recommended
+    fieldsToSet.License = '';
+    fieldsToSet.Authors = {''};
+    fieldsToSet.Acknowledgements = '';
+    fieldsToSet.HowToAcknowledge = '';
+    fieldsToSet.Funding = {''};
+    fieldsToSet.ReferencesAndLinks = {''};
+    fieldsToSet.DatasetDOI = '';
+end
+
+function fieldsToSet = mriJsonDefaults(fieldsToSet)
+
+    % for json for funcfional data
+    % required
+    fieldsToSet.RepetitionTime = [];
+    fieldsToSet.SliceTiming = [];
+    fieldsToSet.TaskName = [];
+    %     fieldsToSet.PhaseEncodingDirection = [];
+    %     fieldsToSet.EffectiveEchoSpacing = [];
+    %     fieldsToSet.EchoTime = [];
+    % recommended
+    fieldsToSet.Instructions = [];
+    fieldsToSet.TaskDescription = [];
 
 end
---
+++ tests/test_checkCFG.m
@@ -0,0 +1,68 @@
+function test_checkCFG()
+
+    fprintf('\n\n--------------------------------------------------------------------\n\n');
+
+    clear;
+
+    outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
+
+    expParameters.subjectNb = 1;
+    expParameters.runNb = 1;
+    expParameters.task = 'testtask';
+    expParameters.outputDir = outputDir;
+
+    expParameters.bids.datasetDescription.Name = 'dummy';
+    expParameters.bids.datasetDescription.BIDSVersion = '1.0.0';
+    expParameters.bids.datasetDescription.Authors = {'Jane Doe', 'John Doe'};
+
+    expParameters.bids.MRI.RepetitionTime = 1.56;
+
+    cfg.testingDevice = 'mri';
+
+    [~, expParameters] = checkCFG(cfg, expParameters);
+
+    %%% test
+
+    % test data
+    expectedStructure.subjectNb = 1;
+    expectedStructure.runNb = 1;
+    expectedStructure.subjectGrp = '';
+    expectedStructure.sessionNb = 1;
+
+    expectedStructure.verbose = 0;
+    expectedStructure.askGrpSess = [true true];
+
+    expectedStructure.outputDir = outputDir;
+
+    expectedStructure.task = 'testtask';
+
+    expectedStructure.MRI.ce = [];
+    expectedStructure.MRI.dir = [];
+    expectedStructure.MRI.rec = [];
+    expectedStructure.MRI.echo = [];
+    expectedStructure.MRI.acq = [];
+
+    expectedStructure.bids.MRI.RepetitionTime = 1.56;
+    expectedStructure.bids.MRI.SliceTiming = '';
+    expectedStructure.bids.MRI.TaskName = 'testtask';
+    expectedStructure.bids.MRI.PhaseEncodingDirection = '';
+    expectedStructure.bids.MRI.EffectiveEchoSpacing = '';
+    expectedStructure.bids.MRI.EchoTime = '';
+    expectedStructure.bids.MRI.Instructions = '';
+    expectedStructure.bids.MRI.TaskDescription = '';
+
+    expectedStructure.bids.datasetDescription.Name = 'dummy';
+    expectedStructure.bids.datasetDescription.BIDSVersion =  '1.0.0';
+    expectedStructure.bids.datasetDescription.License = '';
+    expectedStructure.bids.datasetDescription.Authors = {'Jane Doe', 'John Doe'};
+    expectedStructure.bids.datasetDescription.Acknowledgements = '';
+    expectedStructure.bids.datasetDescription.HowToAcknowledge = '';
+    expectedStructure.bids.datasetDescription.Funding = {''};
+    expectedStructure.bids.datasetDescription.ReferencesAndLinks = {''};
+    expectedStructure.bids.datasetDescription.DatasetDOI = '';
+
+    expectedStructure = orderfields(expectedStructure);
+
+    assert(isequal(expectedStructure, expParameters));
+
+end

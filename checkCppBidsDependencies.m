7ec63b31defea8d069cf703e611fb48bd7d1e4c1
Remi Gau <remi_gau@hotmail.com>
2020-07-23 11:21:33 +0200

    boldFilename

--- checkCppBidsDependencies.m
+++ checkCppBidsDependencies.m
@@ -3,6 +3,7 @@ function checkCppBidsDependencies
     pth = fileparts(mfilename('fullpath'));
     addpath(fullfile(pth, 'lib', 'JSONio'));
     addpath(fullfile(pth, 'lib', 'bids-matlab'));
+    addpath(fullfile(pth, 'lib', 'utils'));
     addpath(fullfile(pth, 'subfun'));
 
 end

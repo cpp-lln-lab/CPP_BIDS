0aa7060f29b815c1ab43f9e84e3c6bf35c9fb4a1
Remi Gau <remi_gau@hotmail.com>
2020-07-28 17:58:36 +0200

    make setDefaultFields recursive and test for it

--- subfun/setDefaultFields.m
+++ subfun/setDefaultFields.m
@@ -1,16 +1,27 @@
 function structure = setDefaultFields(structure, fieldsToSet)
-    % loop through the defaults fiels to set and update if they don't exist
+    % structure = setDefaultFields(structure, fieldsToSet)
+    %
+    % recursively loop through the fields of a structure and sets a value if they don't exist
+    %
 
     names = fieldnames(fieldsToSet);
 
     for i = 1:numel(names)
 
         thisField = fieldsToSet.(names{i});
+        
+        if isfield(structure, names{i}) && isstruct(structure.(names{i}))
+            
+            structure.(names{i}) = ...
+                setDefaultFields(structure.(names{i}), fieldsToSet.(names{i}));
+            
+        else
 
         structure = setFieldToIfNotPresent( ...
             structure, ...
             names{i}, ...
             thisField);
+        end
 
     end
 
---
+++ tests/test_setDefaultFields.m
@@ -0,0 +1,30 @@
+function test_setDefaultFields()
+    
+    structure = struct();
+    
+    fieldsToSet.field = 1;
+    
+    structure = setDefaultFields(structure, fieldsToSet);
+
+    expectedStructure.field = 1;
+    
+    assert(isequal(expectedStructure, structure));
+    
+    fprintf('\n--------------------------------------------------------------------');
+    
+    clear
+    
+    structure.field.subfield_1 = 3;
+    
+    fieldsToSet.field.subfield_1 = 1;
+    fieldsToSet.field.subfield_2 = 1;
+    
+    structure = setDefaultFields(structure, fieldsToSet);
+    
+    expectedStructure.field.subfield_1 = 3;
+    expectedStructure.field.subfield_2 = 1;
+    
+    assert(isequal(expectedStructure, structure));
+
+end
+

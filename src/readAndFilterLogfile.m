b3af2bd348da02d82e076e2db9a6f1452d101daf
marcobarilari <marcobarilr@gmail.com>
2020-09-26 16:19:32 +0200

    mh fix

--- src/readAndFilterLogfile.m
+++ src/readAndFilterLogfile.m
@@ -14,7 +14,7 @@ function outputFiltered = readAndFilterLogfile(columnName, filterBy, saveOutputT
     %  - filterBy: string, the content of the column you want to filter out. It can take just
     %    part of the content name (e.g., you want to display the triggers and you have
     %    'trigger_motion' and 'trigger_static', 'trigger' as input will do)
-    %  - saveOutputTsv: boolean to save the filtered ouput 
+    %  - saveOutputTsv: boolean to save the filtered ouput
     %  - varargin: either cfg (to display the last run output) or the file path as string
     %
     % OUTPUT:
@@ -22,36 +22,35 @@ function outputFiltered = readAndFilterLogfile(columnName, filterBy, saveOutputT
     %  - outputFiltered: dataset with only the specified content, to see it in the command window
     %    use display(outputFiltered)
 
-    
     % Create tag to add to output file in case you want to save it
-    outputFilterTag = [ '_filteredBy-' columnName '_' filterBy '.tsv'];
-    
+    outputFilterTag = ['_filteredBy-' columnName '_' filterBy '.tsv'];
+
     % Checke if input is cfg or the file path and assign the output filename for later saving
     if ischar(varargin{1})
-        
+
         tsvFile = varargin{1};
-        
-        % Divide path and file name 
+
+        % Divide path and file name
         fileSepPos = find(tsvFile == filesep, 1, 'last');
-        path1 = tsvFile(1:fileSepPos-1);
-        path2 = tsvFile(fileSepPos+1:end-4);
-        
+        path1 = tsvFile(1:fileSepPos - 1);
+        path2 = tsvFile(fileSepPos + 1:end - 4);
+
         % Create output file name
         outputFileName = fullfile(path1, ...
-            [ path2 ...
-            outputFilterTag ]);
+                                  [path2 ...
+                                   outputFilterTag]);
 
     elseif isstruct(varargin{1})
-        
+
         tsvFile = fullfile(varargin{1}.dir.outputSubject, ...
                            varargin{1}.fileName.modality, ...
                            varargin{1}.fileName.events);
-        
+
         % Create output file name
         outputFileName = fullfile(varargin{1}.dir.outputSubject, ...
-                           varargin{1}.fileName.modality, ...
-                           [ varargin{1}.fileName.events(1:end-4) ...
-                           outputFilterTag] );
+                                  varargin{1}.fileName.modality, ...
+                                  [varargin{1}.fileName.events(1:end - 4) ...
+                                   outputFilterTag]);
     end
 
     % Check if the file exists
@@ -64,7 +63,7 @@ function outputFiltered = readAndFilterLogfile(columnName, filterBy, saveOutputT
         output = bids.util.tsvread(tsvFile);
     catch
         % Add the 'bids-matlab' in case is not in the path
-        addpath(genpath(fullfile(pwd, '../lib'))) 
+        addpath(genpath(fullfile(pwd, '../lib')));
         % Read the the tsv file and store each column in a field of `output` structure
         output = bids.util.tsvread(tsvFile);
     end
@@ -77,11 +76,11 @@ function outputFiltered = readAndFilterLogfile(columnName, filterBy, saveOutputT
 
     % Get the dataset with the content of intereset
     outputFiltered = outputDataset(filterIdx, :);
-    
+
     if saveOutputTsv
-        
-        bids.util.tsvwrite(outputFileName, output)
-    
+
+        bids.util.tsvwrite(outputFileName, output);
+
     end
-    
+
 end

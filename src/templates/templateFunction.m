9c18de24d983ac0c98dfa02f975fd991b16d189a
Remi Gau <remi_gau@hotmail.com>
2020-11-17 10:42:42 +0100

    add template for unit test

--- src/templates/templateFunction.m
+++ src/templates/templateFunction.m
@@ -17,8 +17,13 @@ function [argout1, argout2] = templateFunction(argin1, argin2, argin3)
     % :param argin3: (dimension) optional argument
     % :type argin3: integer
     %
-    % :returns: - :argout1: (type) (dimension)
-    %           - :argout2: (type) (dimension)
+    % :returns:
+    %           :argout1: (type) (dimension)
+    %           :argout2: (type) (dimension)
+    %
+    % :output:
+    %           :file created: description
+    %           :other file created: description
     %
     % .. todo:
     %

48bd7088b42febe22665603647c55b2ecad3c745
Remi Gau <remi_gau@hotmail.com>
2022-03-10 08:15:39 +0100

    add infra

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, the CPP BIDS dev team'
 author = 'the CPP BIDS dev team'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.0'
+release = 'v2.1.1dev'
 
 
 # -- General configuration ---------------------------------------------------
---
+++ runTests.m
@@ -0,0 +1,39 @@
+function runTests()
+    %
+    % (C) Copyright 2022 CPP_BIDS developers
+
+    % Elapsed time is ??? seconds.
+
+    tic;
+
+    thisPath = fileparts(mfilename('fullpath'));
+
+    cd(thisPath);
+
+    fprintf('\nHome is %s\n', getenv('HOME'));
+
+    warning('OFF');
+
+    addpath(fullfile(thisPath, 'lib', 'JSONio'));
+    addpath(fullfile(thisPath, 'lib', 'bids-matlab'));    
+
+    folderToCover = fullfile(thisPath, 'src');
+    addpath(genpath(folderToCover));
+    testFolder = fullfile(thisPath, 'tests');
+    addpath(genpath(testFolder));
+
+    success = moxunit_runtests(testFolder, ...
+                               '-verbose', '-recursive', '-with_coverage', ...
+                               '-cover', folderToCover, ...
+                               '-cover_xml_file', 'coverage.xml', ...
+                               '-cover_html_dir', fullfile(thisPath, 'coverage_html'));
+
+    if success
+        system('echo 0 > test_report.log');
+    else
+        system('echo 1 > test_report.log');
+    end
+
+    toc;
+
+end

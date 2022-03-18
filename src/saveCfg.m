f234a729199b30462aef6e4eddf7a863db483e54
Remi Gau <remi_gau@hotmail.com>
2022-03-18 17:40:01 +0100

    cover other use case of the API

--- src/saveCfg.m
+++ src/saveCfg.m
@@ -1,19 +1,25 @@
 function filename = saveCfg(varargin)
     %
-    % Saves config as JSON
+    % Saves config as JSON.
     %
     % USAGE::
     %
     %   createJson(cfg [, filename])
     %
-    % :param cfg: Configuration. See ``checkCFG()``.
+    % :param cfg: Required. Configuration. See ``checkCFG()``.
     % :type cfg: structure
     %
-    % :param filename: fullpath filename for the output file.
+    % :param filename: Optional. Fullpath filename for the output file.
     % :type filename: path
     %
-    % :output:
-    %          - filename:
+    % :output: filename
+    %
+    % If a filename is provided, this will be used as an output file (and will
+    % create any required directory)
+    %
+    % If no filename is provided, it will try to create one based on the content
+    % of ``cfg.fileName`` and ``cfg.dir``. If this fails it will save the file
+    % in the ``pwd`` under ``'date-yyyymmddHHMM_cfg.json'``.
     %
     %
     % (C) Copyright 2022 CPP_BIDS developers
@@ -34,13 +40,32 @@ function filename = saveCfg(varargin)
     cfg = args.Results.cfg;
     filename = args.Results.filename;
 
-    filename = strrep(cfg.fileName.events, '_events', '_cfg');
-    filename = strrep(filename, '.tsv', '.json');
-    filename = getFullFilename(filename, cfg);
+    % if no name, get name from config
+    if strcmp(filename, '')
 
-    jsonContent = cfg;
+        if isfield(cfg, 'fileName') && isfield(cfg.fileName, 'events')
+            filename = strrep(cfg.fileName.events, '_events.tsv', '_cfg.json');
+            filename = getFullFilename(filename, cfg);
+        end
+
+    end
+
+    % if this did not work we make one up
+    if strcmp(filename, '')
+        tmp = checkCFG();
+        bf = struct('suffix', 'cfg', ...
+                    'ext', '.json', ...
+                    'entities', struct('date', datestr(now, tmp.fileName.dateFormat)));
+        bf = bids.File(bf);
+        filename = fullfile(pwd, bf.filename);
+    end
+
+    if ~isdir(fileparts(filename))
+        bids.util.mkdir(fileparts(filename));
+    end
 
     %% save
+    jsonContent = cfg;
     bids.util.jsonencode(filename, jsonContent);
 
 end
--- tests/test_saveCfg.m
+++ tests/test_saveCfg.m
@@ -10,9 +10,51 @@ end
 
 function test_saveCfg_basic()
 
-    %% set up
     cfg = setUp();
 
-    filname = saveCfg(cfg);
+    filename = saveCfg(cfg);
+
+    expected = fullfile(cfg.dir.outputSubject, ...
+                        cfg.fileName.modality, ...
+                        strrep(cfg.fileName.events, '_events.tsv', '_cfg.json'));
+
+    assertEqual(filename, expected);
+
+    content = bids.util.jsondecode(filename);
+
+    delete(expected);
+
+end
+
+function test_saveCfg_bare_bone()
+
+    cfg.testingDevice = 'mri';
+
+    filename = saveCfg(cfg);
+
+    expected = fullfile(pwd, ['date-'  datestr(now,  'yyyymmddHHMM') '_cfg.json']);
+
+    assertEqual(filename, expected);
+
+    content = bids.util.jsondecode(filename);
+
+    assertEqual(content, struct('testingDevice', 'mri'));
+
+    delete(expected);
+
+end
+
+function test_saveCfg_filename_provided()
+
+    cfg.testingDevice = 'mri';
+
+    filename = saveCfg(cfg, fullfile(pwd, 'cfg', 'cfg.json'));
+
+    expected = fullfile(pwd, 'cfg', 'cfg.json');
+
+    assertEqual(filename, expected);
+
+    delete(expected);
+    rmdir(fullfile(pwd, 'cfg'));
 
 end

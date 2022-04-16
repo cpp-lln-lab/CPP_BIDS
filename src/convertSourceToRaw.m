bdd0088800f48ab0372d84a9c353b6957a63ae33
Remi Gau <remi_gau@hotmail.com>
2022-04-16 10:33:14 +0200

    refactor removing date entity

--- src/convertSourceToRaw.m
+++ src/convertSourceToRaw.m
@@ -1,4 +1,4 @@
-function convertSourceToRaw(cfg)
+function convertSourceToRaw(varargin)
     %
     % Function attempts to convert a source dataset created with CPP_BIDS into a valid
     % BIDS data set.
@@ -11,6 +11,9 @@ function convertSourceToRaw(cfg)
     % :param cfg: cfg structure is needed only for providing the path in ``cfg.dir.output``.
     % :type cfg: structure
     %
+    % :param filter: bids.query fitler to only convert a subset of files.
+    % :type filter: structure
+    %
     % :output:
     %          - :creates: a dummy README and CHANGE file
     %          - :copies: ``source`` directory to ``raw`` directory
@@ -18,6 +21,17 @@ function convertSourceToRaw(cfg)
     %          - :zips: the ``_stim.tsv`` files.
     %
     % (C) Copyright 2020 CPP_BIDS developers
+
+    args = inputParser;
+
+    default_filter = struct([]);
+
+    args.addRequired('cfg', @isstruct);
+    args.addParameter('filter', default_filter, @isstruct);
+
+    args.parse(varargin{:});
+
+    cfg = args.Results.cfg;
 
     sourceDir = fullfile(cfg.dir.output, 'source');
     rawDir = fullfile(cfg.dir.output, 'raw');
@@ -34,22 +48,7 @@ function convertSourceToRaw(cfg)
 
     copyfile(sourceDir, rawDir);
 
-    BIDS = bids.layout(rawDir,  'use_schema', false);
-
-    data = bids.query(BIDS, 'data');
-    metadata = bids.query(BIDS, 'metadata');
-
-    for i = 1:size(data, 1)
-        bf = bids.File(data{i});
-        if isfield(bf.entities, 'date')
-            % TODO probably JSON renaming should be passed to bids-matlab
-            sourceJson = fullfile(fileparts(bf.path), bf.json_filename);
-            bf.entities.date = '';
-            bf.rename('dry_run', false, 'force', true);
-            bids.util.jsonencode(fullfile(fileparts(bf.path), bf.json_filename), metadata{i});
-            delete(sourceJson);
-        end
-    end
+    removeDateEntity(rawDir);
 
     BIDS = bids.layout(rawDir,  'use_schema', false);
     data = bids.query(BIDS, 'data', 'suffix', {'stim', 'physio' }, 'ext', '.tsv');
@@ -59,6 +58,26 @@ function convertSourceToRaw(cfg)
         if exist(data{i}, 'file')
             delete(data{i});
         end
+    end
+
+end
+
+function removeDateEntity(pathToDataSet)
+
+    BIDS = bids.layout(pathToDataSet,  'use_schema', false);
+
+    filter = struct('date', {'[0-9]+'});
+    data = bids.query(BIDS, 'data', filter);
+    metadata = bids.query(BIDS, 'metadata', filter);
+
+    for i = 1:size(data, 1)
+        bf = bids.File(data{i});
+        % TODO probably JSON renaming should be passed to bids-matlab
+        sourceJson = fullfile(fileparts(bf.path), bf.json_filename);
+        bf.entities.date = '';
+        bf.rename('dry_run', false, 'force', true);
+        bids.util.jsonencode(fullfile(fileparts(bf.path), bf.json_filename), metadata{i});
+        delete(sourceJson);
     end
 
 end
